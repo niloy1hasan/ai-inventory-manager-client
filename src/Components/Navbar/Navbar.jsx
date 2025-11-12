cae338ec192db2cff16c71edd410b7a72938d560
niloy1hasan <notecse23@gmail.com>
2025-11-12 11:00:15 +0600

    logout feature added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,27 @@
-import React, { use } from "react";
-import { NavLink } from "react-router";
+import React, { use, useRef } from "react";
+import { NavLink, useNavigate } from "react-router";
 import { AuthContext } from "../../Context/AuthContext.";
 
 const Navbar = () => {
-  const {user, setUser} = use(AuthContext);
+  const {user, setUser, logoutUser} = use(AuthContext);
+  const navigate = useNavigate();
+
+  console.log(user);
+
+  const logoutModal = useRef();
+
+  const handleLogout = () => {
+    logoutUser()
+    .then(result => {
+      console.log(result);
+      setUser(null);
+      navigate('/login');
+    })
+    .catch(error => {
+      console.log(error);
+    });
+    logoutModal.current.close();
+  }
 
       return (
     <>
@@ -30,7 +48,7 @@ const Navbar = () => {
             </div>
             <ul
               tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-4 shadow"
             >
               <li><a>Home</a></li>
               <li><a>Add Model</a></li>
@@ -47,36 +65,64 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="navbar-end flex gap-2">
-          <NavLink to={'/login'} className="btn rounded-full px-6 bg-blue-600 hover:bg-blue-800 text-white">Login</NavLink>
-        
+    
           {
             user ? 
             <div className="dropdown dropdown-end">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
           <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            alt={user.displayName}
+            src={user.photoURL ? user.photoURL : 'src/assets/default-user.png'} />
         </div>
       </div>
       <ul
         tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        className="menu menu-sm dropdown-content bg-base-100 border rounded-box z-1 mt-3 w-auto p-2 shadow">
+          <div className="flex items-center gap-1.5">
+             <div className="w-8 h-8">
+          <img
+          className="rounded-full"
+            alt={user.displayName}
+            src={user.photoURL ? user.photoURL : 'src/assets/default-user.png'} />
+        </div> 
+
+        <div>
+          <h2 className="font-semibold">{user.displayName}</h2>
+          <p className="text-[12px] text-gray-400">{user.email}</p>
+        </div>
+          </div>
+          <div className="h-px bg-zinc-200 my-2"></div>
         <li>
-          <a className="justify-between">
+          <a>
             Profile
-            <span className="badge">New</span>
-          </a>
+            </a>
         </li>
+        <li><a>Model Purchase</a></li>
+        <li><a>Models</a></li>
+        <div className="h-px bg-zinc-200 my-0.5"></div>
         <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
+        <li><a onClick={()=>{logoutModal.current?.showModal()}}>Logout</a></li>
       </ul>
-    </div> : ''
+    </div> : <NavLink to={'/login'} className="btn rounded-full px-6 bg-blue-600 hover:bg-blue-800 text-white">Login</NavLink>
           }
           
         </div>
       </div>
       </nav>
+
+<dialog ref={logoutModal}  className="modal">
+  <div className="modal-box">
+    <h3 className="font-bold text-lg">Logout</h3>
+    <p className="py-4">Do you want to logout?</p>
+    <div className="modal-action">
+      <form method="dialog" className="flex gap-1.5">
+        <button className="btn">Close</button>
+        <button type="button" onClick={handleLogout} className="btn bg-red-600 text-white hover:bg-red-700">Logout</button>
+      </form>
+    </div>
+  </div>
+</dialog>
     </>
   );
 };
--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext.';
-import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from '../Firebase/firebase.config';
 
 const googleProvider = new GoogleAuthProvider();
@@ -23,6 +23,11 @@ const AuthProvider = ({children}) => {
         return signInWithPopup(auth, googleProvider);
       }
 
+    const logoutUser = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
     useEffect(()=>{
         const unsubscribe = onAuthStateChanged(auth, (currentUser)=>{
             setUser(currentUser);
@@ -40,7 +45,8 @@ const AuthProvider = ({children}) => {
         createUser,
         signInUser,
         loading,
-        googleSignIn
+        googleSignIn,
+        logoutUser
     };
 
     return (
