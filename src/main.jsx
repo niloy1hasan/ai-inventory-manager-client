f556425317a45d149cfacbbe40bc642742b750dc
niloy1hasan <notecse23@gmail.com>
2025-11-25 20:04:52 +0600

    data fetching method changed

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,9 @@ import PrivateRoutes from "./Components/Routes/PrivateRoutes.jsx";
 import AddModel from "./Components/AddModel/AddModel.jsx";
 import AllModels from "./Components/AllModels/AllModels.jsx";
 import ModelDetails from "./Components/ModelDetails/ModelDetails.jsx";
+import UpdateModel from "./Components/UpdateModel/UpdateModel.jsx";
+import MyModels from "./Components/MyModels/MyModels.jsx";
+import PurchasedModel from "./Components/PurchasedModel/PurchasedModel.jsx";
 
 const router = createBrowserRouter([
   {
@@ -22,7 +25,7 @@ const router = createBrowserRouter([
       {
         index: true,
         Component: Home,
-      },
+    },
       {
         path: "/login",
         Component: Login,
@@ -40,11 +43,36 @@ const router = createBrowserRouter([
       {
         path: '/models',
         Component: AllModels,
-        loader: () => fetch('http://localhost:3000/models').then(res => res.json())
       },
       {
-        path: '/model-details',
-        Component: ModelDetails
+        path: '/models/:id',
+        element: 
+        <PrivateRoutes>
+          <ModelDetails></ModelDetails>
+        </PrivateRoutes>,
+        loader: ({ params }) => fetch(`https://ai-inventory-manager-server.vercel.app/models/${params.id}`).then(res => res.json())
+      },
+      {
+        path: '/update-model/:id',
+        element: 
+        <PrivateRoutes>
+          <UpdateModel></UpdateModel>
+        </PrivateRoutes>,
+        loader: ({params}) => fetch(`https://ai-inventory-manager-server.vercel.app/models/${params.id}`).then(res => res.json())
+      },
+      {
+        path: '/my-models',
+        element:
+        <PrivateRoutes>
+          <MyModels></MyModels>
+        </PrivateRoutes>
+      },
+      {
+        path: '/purchased-models',
+        element:
+        <PrivateRoutes>
+          <PurchasedModel></PurchasedModel>
+        </PrivateRoutes>
       },
       {
         path: "/*",
